57b4d12a3d9015b0a67c42e89ab6261bad7c7707
Alia Nguyen <trangkiny@gmail.com>
2022-08-03 12:14:57 +0300

    Generate default template

---
+++ src/server.ts
@@ -0,0 +1,40 @@
+import cookieParser from 'cookie-parser';
+import morgan from 'morgan';
+import helmet from 'helmet';
+import dotenv from "dotenv";
+
+import express, { NextFunction, Request, Response } from 'express';
+import StatusCodes from 'http-status-codes';
+import 'express-async-errors';
+
+import logger from 'jet-logger';
+import { CustomError } from 'src/errors/errors';
+
+dotenv.config()
+const app = express();
+
+// Common middlewares
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+// Show routes called in console during development
+if (process.env.NODE_ENV === 'development') {
+    app.use(morgan('dev'));
+}
+
+// Security (helmet recommended in express docs)
+if (process.env.NODE_ENV === 'production') {
+    app.use(helmet());
+}
+
+// Error handler
+app.use((err: Error | CustomError, _: Request, res: Response, __: NextFunction) => {
+    logger.err(err, true);
+    const status = (err instanceof CustomError ? err.HttpStatus : StatusCodes.BAD_REQUEST);
+    return res.status(status).json({
+        error: err.message,
+    });
+});
+
+export default app;
